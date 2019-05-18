4369a792645a4136acc908bdd9460059d6c4fa71
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-05-19 01:55:07 +0600

    Create selectionSort.cpp

---
+++ Sorting Algorithms/selectionSort.cpp
@@ -0,0 +1,10 @@
+void selectionSort(int arr[], int sizeOfArray){
+    int indexOfSmallest;
+    for(int i=0;i<sizeOfArray-1;i++){
+        indexOfSmallest=i;
+        for(int j=i+1;j<sizeOfArray;j++)
+            if(arr[indexOfSmallest]>arr[j])
+                indexOfSmallest=j;
+        swap(arr[i],arr[indexOfSmallest]);
+    }
+}
