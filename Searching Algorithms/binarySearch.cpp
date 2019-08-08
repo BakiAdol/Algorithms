058bcfc17aa55783446a92b58103a6e5797da9c4
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-08-08 16:50:44 +0600

    Create binarySearch.cpp

---
+++ Searching Algorithms/binarySearch.cpp
@@ -0,0 +1,10 @@
+bool binarySearch(int key,int sizeOfarray, int _array[]){
+    int left=0,right=sizeOfarray-1,mid;
+    while(left<=right){
+        mid = (left+right) >> 1;
+        if(key==_array[mid]) return true;
+        if(key>_array[mid]) left=mid+1;
+        else right=mid-1;
+    }
+    return false;
+}
