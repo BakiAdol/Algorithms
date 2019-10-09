0b8c1d02910359796775180b3c1cb25925218ffa
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-10-10 05:13:50 +0600

    Update and rename mergeSort.cpp to 03_mergeSort.cpp

--- Sorting Algorithms/03_mergeSort.cpp
+++ Sorting Algorithms/03_mergeSort.cpp
@@ -1,7 +1,11 @@
-void mergeS(int arr[], int first, int middle, int last){
+///Complexity O(n log n)
+
+void mergeS(int arr[], int first, int middle, int last)
+{
     int tmpArray[last-first];
     int i=first,j=middle+1,k=-1;
-    while(i<=middle && j<=last){
+    while(i<=middle && j<=last)
+    {
         if(arr[i]<arr[j]) tmpArray[++k]=arr[i++];
         else tmpArray[++k]=arr[j++];
     }
@@ -10,8 +14,11 @@ void mergeS(int arr[], int first, int middle, int last){
     first--;
     for(int i=0;i<=k;i++) arr[++first]=tmpArray[i];
 }
-void mergeSort(int arr[], int first, int last){/// first=0 and last = arr[] last index number
-    if(first<last){
+
+void mergeSort(int arr[], int first, int last) /// first=0 and last = arr[] last index number
+{
+    if(first<last)
+    {
         int middle = (first+last)/2;
         mergeSort(arr,first,middle);
         mergeSort(arr,middle+1,last);
