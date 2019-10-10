4221e466c29970279b5557c677c6b451bc1407f1
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2019-10-10 06:40:24 +0600

    Update 05_STLsort.cpp

--- Sorting Algorithms/05_STLsort.cpp
+++ Sorting Algorithms/05_STLsort.cpp
@@ -31,3 +31,54 @@ int main()
     return 0;
 }
 /-------------------------------------------------------------------
+
+
+/// Sort struct vector    
+    
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ranking
+{
+    int id;
+    int rating;
+};
+
+bool cmp(ranking a, ranking b)
+{
+    if(a.rating>b.rating) return true;
+    else if(a.rating==b.rating)
+    {
+        if(a.id<b.id) return true;
+        return false;
+    }
+    return false;
+}
+
+int main()
+{
+    vector<ranking> problemSolver;
+    vector<ranking>::iterator it;
+    ranking r;
+    r.id=5, r.rating=50;
+    problemSolver.push_back(r);
+    r.id=2, r.rating=60;
+    problemSolver.push_back(r);
+    r.id=3, r.rating=50;
+    problemSolver.push_back(r);
+    r.id=4, r.rating=60;
+    problemSolver.push_back(r);
+    r.id=1, r.rating=50;
+    problemSolver.push_back(r);
+
+    sort(problemSolver.begin(), problemSolver.end(),cmp);
+
+    for(it=problemSolver.begin(); it!=problemSolver.end();it++)
+    {
+        cout << "ID : " << (*it).id  << ", Rating : " << (*it).rating << endl;
+    }
+
+    return 0;
+}
+
