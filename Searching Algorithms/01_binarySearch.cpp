3f3601dacfbecf365befb7936475feb806160b1a
Abdullahil Baki Adol <41238152+BakiAdol@users.noreply.github.com>
2020-01-30 15:13:49 +0600

    Update 01_binarySearch.cpp

--- Searching Algorithms/01_binarySearch.cpp
+++ Searching Algorithms/01_binarySearch.cpp
@@ -1,7 +1,8 @@
 bool binarySearch(int key,int sizeOfarray, int _array[]){
     int left=0,right=sizeOfarray-1,mid;
     while(left<=right){
-        mid = (left+right) >> 1;
+        //mid = (left+right) >> 1;
+        mid = left + (right-left)/2;
         if(key==_array[mid]) return true;
         if(key>_array[mid]) left=mid+1;
         else right=mid-1;
